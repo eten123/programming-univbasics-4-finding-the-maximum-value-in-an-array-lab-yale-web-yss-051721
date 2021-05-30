e645f36393b4ba1df7fa50d6a89c520b96dabe45
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 19:03:35 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,8 @@ def find_max_value(array)
   count = 0 
   max_value = -1
   while count < array.length do
-   if array[count] < array[coun]
+   if array[count] < array[-1]
+     array.
   end
   
 end
