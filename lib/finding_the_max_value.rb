c4c8b1b5ea85e39204dc9a084925c95ef22df5bb
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 19:00:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,10 @@
 def find_max_value(array)
   # Add your solution here
   count = 0 
+  new_array = []
   array.first
   
+famous_cats.unshift("nala cat")
   while count < array.length do
     
    if array[count] < 
